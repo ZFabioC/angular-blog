816d0ef053684bba57acc7f3b753469d04328e81
Fabio <fabio_zanella00@gmail.com>
2023-09-21 22:46:12 -0300

    conteudo criado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Novidades no Angular",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure corrupti accusamus magni provident quidem laborum, beatae facilis? Laborum voluptate debitis vel cupiditate aperiam nostrum dignissimos expedita quos, aut nesciunt illum.",
+        "photoCover": "https://blog.back4app.com/wp-content/uploads/2020/09/angular-backend-2-2.png"
+    },
+    {
+        "id": "2",
+        "title": "React Native",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure corrupti accusamus magni provident quidem laborum, beatae facilis? Laborum voluptate debitis vel cupiditate aperiam nostrum dignissimos expedita quos, aut nesciunt illum.",
+        "photoCover": "https://miro.medium.com/v2/resize:fit:1024/1*QY5S4senfFh-mIViSi5A_Q.png" 
+    },
+    {
+        "id": "3",
+        "title": "React",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure corrupti accusamus magni provident quidem laborum, beatae facilis? Laborum voluptate debitis vel cupiditate aperiam nostrum dignissimos expedita quos, aut nesciunt illum.",
+        "photoCover": "https://www.freecodecamp.org/portuguese/news/content/images/2023/03/Ekran-Resmi-2019-11-18-18.08.13.png"
+    },
+    {
+        "id": "4",
+        "title": "Vue",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure corrupti accusamus magni provident quidem laborum, beatae facilis? Laborum voluptate debitis vel cupiditate aperiam nostrum dignissimos expedita quos, aut nesciunt illum.",
+        "photoCover": "https://www.vectorlogo.zone/logos/vuejs/vuejs-ar21.png" 
+    }
+]
